d88c65d6639f4245379c580f2852a39dc598c3d7
Student11 <zado05062011@gmail.com>
2017-06-12 14:23:23 +0700

    variable

---
+++ variabal and constant.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// this comment หรือ นี้คือ comment
+// นี่คือการประการตัวแปรแบบเปลี่ยนค่าไม่ได้
+let MyConstant = 123
+// myconstant = 456 ไม่สามารถเปลี่ยนค่าของ let ได้
+
+// นี่คือการประกาศตัวแปรแบบเปลี่ยนค่าได้
+var intnumber = 123 // implicit variabal inNumber = 123
+intnumber = 456
+
+// การทำ explicit การประกาศตัวแปรที่ระบุ datatype
+let MyConstant1 : Int = 123
