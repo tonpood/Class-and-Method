667c3aea199eab25199ce62ca88961ae96094f43
Student11 <zado05062011@gmail.com>
2017-06-13 12:54:20 +0700

    กำหนดตัวแปร

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การใช้ตัวแปร  Array
+//การประกาศตัวแปร Array และการกำหนดค่า
+
+//Implicit ประกาศโดยให้รู้ว่านี้คืออะไร
+let intArrayNum1 = [11,22,33,44]
+
+//นี้คือการประกาศ แบบ Explicit
+var intArrayNum2 = [Int]()
+intArrayNum2 = [55,66,77,88]
+
+//ประกาศตัวแปรแบบ ทศนิยม
+var intArrayNum3 = [Double]()
+intArrayNum3 = [1.2,1.3,1.4,1.5]
+
+//ประกาศตัวแปรแบบ ตัวอักษร
+var intArrayNum4 = [String]()
+intArrayNum4 = ["ต้น","ทดสอบ","สุดยอด"]
+
+		
