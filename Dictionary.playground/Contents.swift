59d4a5ee07ddc5aa79b360cb051ad10b6b648632
Student11 <zado05062011@gmail.com>
2017-06-13 14:07:18 +0700

    Start Array Dictionary

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//กำหนดค่าให้กับ Dictionary
+
+var strDicNane = ["Android": "นี้คือ OS ของมือถือแอนดรอยด์", "IOS": "นี้คือ OS ของมือถือ IPhones"]
+
