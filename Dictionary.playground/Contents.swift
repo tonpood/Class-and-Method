7e4fccb460e3f720fdabd45594bfae2289608e39
Student11 <zado05062011@gmail.com>
2017-06-13 14:11:17 +0700

    การนำไปใช้

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -4,5 +4,8 @@ import UIKit
 
 //กำหนดค่าให้กับ Dictionary
 
-var strDicNane = ["Android": "นี้คือ OS ของมือถือแอนดรอยด์", "IOS": "นี้คือ OS ของมือถือ IPhones"]
+var strDicNane = ["Android": "OS ของมือถือแอนดรอยด์", "IOS": "OS ของมือถือ IPhones"]
 
+//How to Use?
+strDicNane["IOS"]
+print("IOS คือ \(String(describing: strDicNane["IOS"]!))")
