bf184da6f010bb9b926ff9c2d5be54c8a60768e8
Student11 <zado05062011@gmail.com>
2017-06-13 15:08:49 +0700

    End

--- Optional.playground/Contents.swift
+++ Optional.playground/Contents.swift
@@ -3,7 +3,20 @@
 import UIKit
 
 //Optional คืออะไร ?
-var intNumber: Int = 123
+var intNumber: Int?
 
-print("intNumber ==> \(intNumber)")
+print("intNumber ==> \(String(describing: intNumber))")
 
+//แนวทางการใช้ Optional 
+
+var strnumber = "5"
+
+strnumber = "10"
+strnumber = "Ten"
+
+var intnumber1 = Int(strnumber)
+
+if var intmynumber = intnumber1 {
+    var myanswer = intmynumber
+    print("intnumber1 *2 ==> \(myanswer * 2)")
+}
